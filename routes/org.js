b7470fad1593c68e97527bbebf11a309935f30b4
wsjyz@aliyun.com <6148414a>
2014-08-19 18:44:57 +0800

    添加接口

--- routes/org.js
+++ routes/org.js
@@ -53,10 +53,7 @@ router.get('/totalCount/:type',function(req, res,next){
     })
 });
 
-//赞助企业入驻
-router.get('/to-orgSettled', function(req, res) {
-    res.render('orgSettled', { title: '赞助企业入驻' });
-});
+
 
 
 module.exports = router;
